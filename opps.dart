91cd88084706f9820f813fcd538081aa4fddf711
SuprimTamang <suprimtamang123456789@gmail.com>
2022-08-01 07:48:20 +0545

    test

---
+++ opps.dart
@@ -0,0 +1,29 @@
+//oop keyword
+// class , extends
+// this , super ,with , override , abstract
+
+class Mobile {
+  // member properties
+  String display, camera, processor;
+  Mobile(
+      {required this.display, required this.camera, required this.processor});
+
+  details() {
+    print("${this.display}+${this.camera}+${this.processor}");
+  }
+}
+
+//inhertance
+//abstraction
+//polymorphism
+//encapsulation
+main() {
+  Mobile iphone = Mobile(display: "retina", camera: "12mp", processor: "a13");
+  Mobile samsung = Mobile(display: "led", camera: "50mp", processor: "ryzen");
+  Mobile oneplus =
+      Mobile(display: "liquid", camera: "10mp", processor: "ryzen");
+
+  iphone.details();
+  samsung.details();
+  oneplus.details();
+}
