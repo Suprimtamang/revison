f49dad57202d1b97bcd8564e8deaabd20c95b9a5
SuprimTamang <suprimtamang123456789@gmail.com>
2022-08-01 07:46:18 +0545

    learning oops

--- list_map.dart
+++ list_map.dart
@@ -10,21 +10,21 @@ main() {
   List<int> rollNo = [1, 2, 3, 4];
   rollNo.reversed;
   print("the reversed roll no are: ${rollNo.reversed}");
+
+  Map<String, dynamic> person = {
+    "profilePicture": "link of that pp",
+    "age": 10,
+
+    /*while using map there must be a key and a value 
+  for eg here age is key & 10 is a value */
+  };
+//map function in a map person
+  person.map(
+    (key, value) {
+      print("key is :$key & value is $value");
+      return MapEntry(key, value);
+    },
+  );
 }
 // Map = data type
 // map function available in both list , set & map
-
-Map<String, dynamic> person = {
-  "profilePicture": "link of that pp",
-  "age": 10,
-
-  /*while using map there must be a key and a value 
-  for eg here age is key & 10 is a value */
-};
-//map function in a map person 
-/*person.map(
-  (key,value){
-    print("key is :$key & value is $value");
-return MapEntry(key,value);
-  },
-);*/
